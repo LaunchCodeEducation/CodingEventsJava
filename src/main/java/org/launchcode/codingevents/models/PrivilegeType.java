dd6083a398d723011dcd5baa578d244f7039e7f8
Ben Clark <brclark15@gmail.com>
2023-09-12 11:49:37 -0500

    Define Roles & Privileges models for User
    - Add Roles and RoleTypes
    - Add Privilege and PrivilegeTypes
    - Add relationship to User

---
+++ src/main/java/org/launchcode/codingevents/models/PrivilegeType.java
@@ -0,0 +1,10 @@
+package org.launchcode.codingevents.models;
+
+public enum PrivilegeType {
+    READ_EVENTS,
+    CREATE_EVENTS,
+    DELETE_EVENTS,
+    READ_USERS,
+    UPDATE_USERS,
+    DELETE_USERS
+}
--- src/main/java/org/launchcode/codingevents/models/User.java
+++ src/main/java/org/launchcode/codingevents/models/User.java
@@ -1,9 +1,21 @@
 package org.launchcode.codingevents.models;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "username"))
 public class User extends AbstractEntity {
 
     @NotNull
@@ -12,11 +24,33 @@ public class User extends AbstractEntity {
     @NotNull
     private String pwHash;
 
+    private LocalDateTime createDate;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name="users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles;
+
     public User() {}
 
     public User(String username, String pwHash) {
         this.username = username;
         this.pwHash = pwHash;
+    }
+
+    public User(String username, String pwHash, Collection<Role> roles) {
+        this.username = username;
+        this.pwHash = pwHash;
+        this.roles = roles;
+    }
+
+    @PrePersist
+    public void setUpCreateDate() {
+        createDate = LocalDateTime.now();
     }
 
     public String getUsername() {
@@ -25,5 +59,13 @@ public class User extends AbstractEntity {
 
     public String getPwHash() {
         return pwHash;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
     }
 }
